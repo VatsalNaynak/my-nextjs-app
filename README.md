cdf502a87b127bd96ad137ed3dddb0fbf3a2b201
VatsalNaynak <vattyboy7@gmail.com>
2022-01-04 15:52:06 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nextjs-krxzd7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nextjs-krxzd7)
---
+++ pages/imagecomponent.js
@@ -0,0 +1,7 @@
+export default function ImageComponent() {
+  return (
+    <>
+      <img src="https://images.unsplash.com/photo-1641251379998-858837c527c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" />
+    </>
+  );
+}
