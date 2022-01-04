5d52052c93429734323e3824f73c885869c0aed4
VatsalNaynak <vattyboy7@gmail.com>
2022-01-04 16:20:23 +0530

    image changes

--- pages/imagecomponent.js
+++ pages/imagecomponent.js
@@ -1,7 +1,7 @@
 export default function ImageComponent() {
   return (
     <>
-      <img src="../images/image1.jpg" />
+      <img src="https://random.dog/7d76ab15-5cc1-4684-aa82-568617fed752.jpg" />
       <img src="../images/image2.jpg" />
       <img src="../images/image3.jpg" />
     </>
