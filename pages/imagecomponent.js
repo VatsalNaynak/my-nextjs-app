5d239a4b8dc239f05a9c70bebe4e9f31f97627b8
VatsalNaynak <vattyboy7@gmail.com>
2022-01-04 16:05:15 +0530

    Images added

--- pages/imagecomponent.js
+++ pages/imagecomponent.js
@@ -2,6 +2,8 @@ export default function ImageComponent() {
   return (
     <>
       <img src="../images/image1.jpg" />
+      <img src="../images/image2.jpg" />
+      <img src="../images/image3.jpg" />
     </>
   );
 }
